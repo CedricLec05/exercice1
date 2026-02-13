c5173a9da0167164934ca15fe909d7be6c0de744
CedricLec05 <cedric.leclerc123@gmail.com>
2026-02-13 09:32:15 -0500

    initial

---
+++ Assets/scripts/ObstacleBouge.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class ObstacleBouge : MonoBehaviour
+{
+    public float posX;
+    public int vitesse = 2;
+
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // posXRando = new Vector2(Random.Range(-10f, 10f));
+        // Debug.Log(posXRando);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
